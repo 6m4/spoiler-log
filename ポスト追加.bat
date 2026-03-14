edc08b3c31afc02330918e810c5a5695285bfe83
tabetabe <tabetabeusagi@gmail.com>
2026-03-14 17:58:50 +0900

    ポスト追加

--- ポスト追加.bat
+++ ポスト追加.bat
@@ -1,22 +1,30 @@
 @echo off
 setlocal
 
+rem 投稿先の作品slugをここだけ変える
+rem 作品追加はD:\ネタバレ保管庫\src\_data\works.jsonを修正
+set "WORK_SLUG=paranomaise"
+
 for /f "delims=" %%i in ('powershell -NoProfile -ExecutionPolicy Bypass -Command "([DateTimeOffset]::Now).ToString('yyyy-MM-ddTHH:mm:sszzz')"') do set "DATETEXT=%%i"
 for /f "delims=" %%i in ('powershell -NoProfile -ExecutionPolicy Bypass -Command "(Get-Date).ToString('yyyy-MM-dd-HHmmss')"') do set "STAMP=%%i"
 
-set "FILE=%~dp0%STAMP%.md"
+set "OUTDIR=%~dp0src\posts"
+set "BASENAME=%STAMP%-%WORK_SLUG%"
+set "FILE=%OUTDIR%\%BASENAME%.md"
+
+if not exist "%OUTDIR%" mkdir "%OUTDIR%"
 
 (
 echo ---
 echo tags:
 echo   - posts
 echo date: %DATETEXT%
-echo work: "blueprince"
+echo work: "%WORK_SLUG%"
 echo ---
 echo.
 echo Write here.
 echo.
-echo ^![](/images/%STAMP%.jpg^)
+echo ^![](/images/%BASENAME%.jpg^)
 echo.
 echo {%% youtube "https://www.youtube.com/watch?v=xxxxxxxxxxx" %%}
 ) > "%FILE%"
